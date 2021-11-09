d345a8bd0602262f796151706045526d69813f33
Akeem <AkeemS11788@gmail.com>
2021-11-09 13:39:56 -0500

    commit tree 4c96768b91c47e264cf53e6e8524affb669655d9

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+
+    document.addEventListener('DOMContentLoaded', () => { //add event listener for DOMContentLoaded that calls on
+        updateText()                                      //updateText function
+        });
+    
+
+
+
+
+        function updateText (){
+    document.getElementById("text").textContent = "This is really cool!"  
+}    //created function that updates text for element id Text
